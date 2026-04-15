12fddb98374bef9c026298748c5d324e700a4321
Rodrigo Pereira <rodrigopereira.development@gmail.com>
2026-04-15 19:32:23 -0300

    update raspador coletor_links

--- coletor_links.py
+++ coletor_links.py
@@ -4,18 +4,32 @@ import sqlite3
 
 def coletar_seletivo():
     categorias = {
+       # Mercearia
         "Arroz": "https://www.goodbom.com.br/goodbom-mogi-mirim-sp/mercearia-basica-arroz-5/",
         "Feijão": "https://www.goodbom.com.br/goodbom-mogi-mirim-sp/mercearia-basica-feijao-78/",
         "Óleo": "https://www.goodbom.com.br/goodbom-mogi-mirim-sp/mercearia-basica-oleo-24/",
         "Café": "https://www.goodbom.com.br/goodbom-mogi-mirim-sp/mercearia-basica-cafe-76/",
         "Macarrão": "https://www.goodbom.com.br/goodbom-mogi-mirim-sp/mercearia-basica-massas-81/",
+        "Farinaceos-grãos": "https://www.goodbom.com.br/goodbom-mogi-mirim-sp/mercearia-salgada-farinaceos-graos-44",
+        
+        # Perecíveis
         "Hortifruti": "https://www.goodbom.com.br/goodbom-mogi-mirim-sp/hortifrutigranjeiro-1/",
         "Açougue": "https://www.goodbom.com.br/goodbom-mogi-mirim-sp/acougue-47/",
+        "Peixaria": "https://www.goodbom.com.br/goodbom-mogi-mirim-sp/peixaria-82",
         "Leite": "https://www.goodbom.com.br/goodbom-mogi-mirim-sp/leites-iogurtes-e-achocolatados-leites-62/",
+        "Ovos": "https://www.goodbom.com.br/goodbom-mogi-mirim-sp/ovos-91/",
+        "Padaria": "https://www.goodbom.com.br/goodbom-mogi-mirim-sp/padaria-50",
+        
+        # Higiene e Limpeza
         "Limpeza": "https://www.goodbom.com.br/goodbom-mogi-mirim-sp/limpeza-122/",
-        "Higiene": "https://www.goodbom.com.br/goodbom-mogi-mirim-sp/higiene-e-cuidados-pessoais-102/"
-    }
+        "Higiene": "https://www.goodbom.com.br/goodbom-mogi-mirim-sp/higiene-e-cuidados-pessoais-102/",
 
+        # Pet Shop
+        "Pet Shop": "https://www.goodbom.com.br/goodbom-mogi-mirim-sp/pet-shop-14",
+
+        # Magazine
+        "Magazine": "https://www.goodbom.com.br/goodbom-mogi-mirim-sp/magazine-16"
+    }
     # Cria o banco de dados local na hora
     conn = sqlite3.connect('arca.db')
     cursor = conn.cursor()
